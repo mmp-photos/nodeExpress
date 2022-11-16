d8f296b9a9aa41c8fd93a64bd83eec310ae96fd6
Matthew Mayer <grafixgeek@yahoo.com>
2022-11-16 18:42:14 -0500

    Passport Authentication - Sessions

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const usersRouter = require('./routes/users');
 const campsitesRouter = require('./routes/campsitesRouter')
 const promotionRouter = require('./routes/promotionRouter')
 const partnerRouter = require('./routes/partnerRouter')
@@ -32,11 +36,43 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser('cookie-is-sometimes-food'));
 
+// SET UP SESSION //
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// ROUTES FOR ENDPOINTS THAT DON'T REQUIRE AUTHENTICATION //
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// CHECK FOR AUTHENTICATION //
+function auth(req, res, next) {
+  console.log(req.user);
+
+  if(!req.user) {
+      const err = new Error('You are not authenticated');
+      err.status = 401;
+      return next(err);
+    } else {
+        return next();
+    }
+};
+
+
+app.use(auth);
+
+// BEGIN APP SPECIFIC ROUTES THAT REQUIRE AUTHENTICATION //
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/campsites', campsitesRouter);
 app.use('/partner', partnerRouter);
 app.use('/promotion', promotionRouter);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,93 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const User = require('../models/user');
 
-/* GET users listing. */
+const router = express.Router();
+const passport = require('passport');
+
+// GET USERS LISTING //
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+// POST USER SIGNUP //
+router.post('/signup', (req, res) => {
+    User.register(
+      new User({username: req.body.username}),
+      req.body.password,
+      err => {
+        if(err){
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({err: err})
+        } else {
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({success: true, status: 'Registration Successful'});
+          })
+        }
+      }
+    )
+});
+
+// POST USER LOGIN //
+router.post('/login', passport.authenticate('local'), (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ success: true, status: 'You are successfully logged in!'});
+
+  // COMMENTING OUT TO PRESERVE THE CODE FOR REFERENCE - FUNCTIONALITY WAS REPLACED BY THE CODE ABOVE //
+  /*
+  if(!req.session.user) {
+      const authHeader = req.headers.authorization;
+
+      if (!authHeader) {
+          const err = new Error('You are not authenticated!');
+          res.setHeader('WWW-Authenticate', 'Basic');
+          err.status = 401;
+          return next(err);
+      }
+    
+      const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+      const username = auth[0];
+      const password = auth[1];
+    
+      User.findOne({username: username})
+      .then(user => {
+          if (!user) {
+              const err = new Error(`User ${username} does not exist!`);
+              err.status = 401;
+              return next(err);
+          } else if (user.password !== password) {
+              const err = new Error('Your password is incorrect!');
+              err.status = 401;
+              return next(err);
+          } else if (user.username === username && user.password === password) {
+              req.session.user = 'authenticated';
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'text/plain');
+              res.end('You are authenticated!')
+          }
+      })
+      .catch(err => next(err));
+  } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('You are already authenticated!');
+  }
+*/
+});
+
+// GET USER SIGNOUT //
+router.post('/logout', (req, res, next) => {
+    if(req.session) {
+        req.session.destroy();
+        res.clearCookie('session.id');
+        res.redirect('/');
+    } else {
+        const err = new Error('You are not logged in');
+        return next(err);
+    }
 });
 
 module.exports = router;
